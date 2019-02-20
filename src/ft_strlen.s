2a07e296a1f03e51a1024aa666530306ce5a5e07
Fanny HUANG <fhuang@e2r4p20.42.fr>
2019-02-20 17:18:27 +0100

    ft_strlen using repeat instruction

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -5,21 +5,27 @@
 	global _ft_strlen
 	section .text
 _ft_strlen:
-	push rbp
-	mov rbp, rsp
-	mov rsi, rdi			; rdi to rsi --> same address
+	push	rbp
+	mov		rbp, rsp
 
-next:
-	cmp rdi, 0				; cmp rdi to NULL pointer
-	je finish				; if TRUE, jump to finish
-	cmp byte [rdi], 0		; cmp *rdi to \0
-	je finish				; if TRUE, jump to finish
-	inc rdi					; increment rdi -> ++rdi
-	jmp next				; continue
+	cmp		rdi, 0x0
+	je		null_pointer
 
-finish:
-	sub rdi, rsi			; substract -> (rsi - rdi). value stored in rdi
-	mov rax, rdi			; mov rdi value in rax --> rax = return value
-	mov rsp, rbp
-	pop rbp
+	mov		rsi, rdi
+	mov		rax, 0x0
+	repne	scasb
+
+	sub		rdi, rsi			; substract -> (rsi - rdi). value stored in rdi
+	sub		rdi, 1			; substract -> (rsi - rdi). value stored in rdi
+	mov		rax, rdi			; mov rdi value in rax --> rax = return value
+
+	mov		rsp, rbp
+	pop		rbp
+
+	ret
+
+null_pointer:
+	mov		rax, 0x0
+	mov		rsp, rbp
+	pop		rbp
 	ret
